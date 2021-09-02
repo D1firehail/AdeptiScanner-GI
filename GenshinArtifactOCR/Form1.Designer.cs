43ba0afe93884b4338b4986e00448004450246ad
D1firehail <danne.firehail@gmail.com>
2021-09-02 22:24:34 +0200

    Basic window, capture and filter

--- GenshinArtifactOCR/Form1.Designer.cs
+++ GenshinArtifactOCR/Form1.Designer.cs
@@ -54,16 +54,21 @@ namespace GenshinArtifactOCR
             // 
             // image_preview
             // 
+            this.image_preview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.image_preview.Location = new System.Drawing.Point(12, 12);
+            this.image_preview.MinimumSize = new System.Drawing.Size(100, 100);
             this.image_preview.Name = "image_preview";
-            this.image_preview.Size = new System.Drawing.Size(390, 552);
-            this.image_preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.image_preview.Size = new System.Drawing.Size(384, 509);
+            this.image_preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.image_preview.TabIndex = 1;
             this.image_preview.TabStop = false;
             // 
             // text_Set
             // 
-            this.text_Set.Location = new System.Drawing.Point(483, 37);
+            this.text_Set.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_Set.Location = new System.Drawing.Point(493, 12);
             this.text_Set.Name = "text_Set";
             this.text_Set.ReadOnly = true;
             this.text_Set.Size = new System.Drawing.Size(279, 20);
@@ -71,7 +76,8 @@ namespace GenshinArtifactOCR
             // 
             // text_Type
             // 
-            this.text_Type.Location = new System.Drawing.Point(483, 63);
+            this.text_Type.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_Type.Location = new System.Drawing.Point(493, 38);
             this.text_Type.Name = "text_Type";
             this.text_Type.ReadOnly = true;
             this.text_Type.Size = new System.Drawing.Size(279, 20);
@@ -79,7 +85,8 @@ namespace GenshinArtifactOCR
             // 
             // text_statMain
             // 
-            this.text_statMain.Location = new System.Drawing.Point(483, 89);
+            this.text_statMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_statMain.Location = new System.Drawing.Point(493, 64);
             this.text_statMain.Name = "text_statMain";
             this.text_statMain.ReadOnly = true;
             this.text_statMain.Size = new System.Drawing.Size(279, 20);
@@ -87,7 +94,8 @@ namespace GenshinArtifactOCR
             // 
             // text_statSub1
             // 
-            this.text_statSub1.Location = new System.Drawing.Point(483, 115);
+            this.text_statSub1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_statSub1.Location = new System.Drawing.Point(493, 90);
             this.text_statSub1.Name = "text_statSub1";
             this.text_statSub1.ReadOnly = true;
             this.text_statSub1.Size = new System.Drawing.Size(279, 20);
@@ -95,7 +103,8 @@ namespace GenshinArtifactOCR
             // 
             // text_statSub2
             // 
-            this.text_statSub2.Location = new System.Drawing.Point(483, 141);
+            this.text_statSub2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_statSub2.Location = new System.Drawing.Point(493, 116);
             this.text_statSub2.Name = "text_statSub2";
             this.text_statSub2.ReadOnly = true;
             this.text_statSub2.Size = new System.Drawing.Size(279, 20);
@@ -103,7 +112,8 @@ namespace GenshinArtifactOCR
             // 
             // text_statSub3
             // 
-            this.text_statSub3.Location = new System.Drawing.Point(483, 167);
+            this.text_statSub3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_statSub3.Location = new System.Drawing.Point(493, 142);
             this.text_statSub3.Name = "text_statSub3";
             this.text_statSub3.ReadOnly = true;
             this.text_statSub3.Size = new System.Drawing.Size(279, 20);
@@ -111,7 +121,8 @@ namespace GenshinArtifactOCR
             // 
             // text_statSub4
             // 
-            this.text_statSub4.Location = new System.Drawing.Point(483, 193);
+            this.text_statSub4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_statSub4.Location = new System.Drawing.Point(493, 168);
             this.text_statSub4.Name = "text_statSub4";
             this.text_statSub4.ReadOnly = true;
             this.text_statSub4.Size = new System.Drawing.Size(279, 20);
@@ -119,8 +130,9 @@ namespace GenshinArtifactOCR
             // 
             // label_Set
             // 
+            this.label_Set.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_Set.AutoSize = true;
-            this.label_Set.Location = new System.Drawing.Point(454, 44);
+            this.label_Set.Location = new System.Drawing.Point(464, 19);
             this.label_Set.Name = "label_Set";
             this.label_Set.Size = new System.Drawing.Size(23, 13);
             this.label_Set.TabIndex = 8;
@@ -128,8 +140,9 @@ namespace GenshinArtifactOCR
             // 
             // label_Type
             // 
+            this.label_Type.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_Type.AutoSize = true;
-            this.label_Type.Location = new System.Drawing.Point(446, 70);
+            this.label_Type.Location = new System.Drawing.Point(456, 45);
             this.label_Type.Name = "label_Type";
             this.label_Type.Size = new System.Drawing.Size(31, 13);
             this.label_Type.TabIndex = 9;
@@ -137,8 +150,9 @@ namespace GenshinArtifactOCR
             // 
             // label_statMain
             // 
+            this.label_statMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_statMain.AutoSize = true;
-            this.label_statMain.Location = new System.Drawing.Point(425, 96);
+            this.label_statMain.Location = new System.Drawing.Point(435, 71);
             this.label_statMain.Name = "label_statMain";
             this.label_statMain.Size = new System.Drawing.Size(52, 13);
             this.label_statMain.TabIndex = 10;
@@ -146,8 +160,9 @@ namespace GenshinArtifactOCR
             // 
             // label_statSub1
             // 
+            this.label_statSub1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_statSub1.AutoSize = true;
-            this.label_statSub1.Location = new System.Drawing.Point(425, 122);
+            this.label_statSub1.Location = new System.Drawing.Point(435, 97);
             this.label_statSub1.Name = "label_statSub1";
             this.label_statSub1.Size = new System.Drawing.Size(52, 13);
             this.label_statSub1.TabIndex = 11;
@@ -155,8 +170,9 @@ namespace GenshinArtifactOCR
             // 
             // label_statSub2
             // 
+            this.label_statSub2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_statSub2.AutoSize = true;
-            this.label_statSub2.Location = new System.Drawing.Point(425, 148);
+            this.label_statSub2.Location = new System.Drawing.Point(435, 123);
             this.label_statSub2.Name = "label_statSub2";
             this.label_statSub2.Size = new System.Drawing.Size(52, 13);
             this.label_statSub2.TabIndex = 12;
@@ -164,8 +180,9 @@ namespace GenshinArtifactOCR
             // 
             // label_statSub3
             // 
+            this.label_statSub3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_statSub3.AutoSize = true;
-            this.label_statSub3.Location = new System.Drawing.Point(425, 174);
+            this.label_statSub3.Location = new System.Drawing.Point(435, 149);
             this.label_statSub3.Name = "label_statSub3";
             this.label_statSub3.Size = new System.Drawing.Size(52, 13);
             this.label_statSub3.TabIndex = 13;
@@ -173,8 +190,9 @@ namespace GenshinArtifactOCR
             // 
             // label_statSub4
             // 
+            this.label_statSub4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_statSub4.AutoSize = true;
-            this.label_statSub4.Location = new System.Drawing.Point(425, 200);
+            this.label_statSub4.Location = new System.Drawing.Point(435, 175);
             this.label_statSub4.Name = "label_statSub4";
             this.label_statSub4.Size = new System.Drawing.Size(52, 13);
             this.label_statSub4.TabIndex = 15;
@@ -182,25 +200,30 @@ namespace GenshinArtifactOCR
             // 
             // btn_capture
             // 
-            this.btn_capture.Location = new System.Drawing.Point(428, 541);
+            this.btn_capture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_capture.Location = new System.Drawing.Point(535, 516);
             this.btn_capture.Name = "btn_capture";
             this.btn_capture.Size = new System.Drawing.Size(75, 23);
             this.btn_capture.TabIndex = 16;
             this.btn_capture.Text = "Capture";
             this.btn_capture.UseVisualStyleBackColor = true;
+            this.btn_capture.Click += new System.EventHandler(this.btn_capture_Click);
             // 
             // btn_Filter
             // 
-            this.btn_Filter.Location = new System.Drawing.Point(509, 541);
+            this.btn_Filter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Filter.Location = new System.Drawing.Point(616, 516);
             this.btn_Filter.Name = "btn_Filter";
             this.btn_Filter.Size = new System.Drawing.Size(75, 23);
             this.btn_Filter.TabIndex = 17;
             this.btn_Filter.Text = "Filter";
             this.btn_Filter.UseVisualStyleBackColor = true;
+            this.btn_Filter.Click += new System.EventHandler(this.btn_Filter_Click);
             // 
             // btn_OCR
             // 
-            this.btn_OCR.Location = new System.Drawing.Point(590, 541);
+            this.btn_OCR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_OCR.Location = new System.Drawing.Point(697, 516);
             this.btn_OCR.Name = "btn_OCR";
             this.btn_OCR.Size = new System.Drawing.Size(75, 23);
             this.btn_OCR.TabIndex = 18;
@@ -209,7 +232,8 @@ namespace GenshinArtifactOCR
             // 
             // text_raw
             // 
-            this.text_raw.Location = new System.Drawing.Point(483, 229);
+            this.text_raw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.text_raw.Location = new System.Drawing.Point(493, 204);
             this.text_raw.Multiline = true;
             this.text_raw.Name = "text_raw";
             this.text_raw.ReadOnly = true;
@@ -218,8 +242,9 @@ namespace GenshinArtifactOCR
             // 
             // label_raw
             // 
+            this.label_raw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_raw.AutoSize = true;
-            this.label_raw.Location = new System.Drawing.Point(448, 229);
+            this.label_raw.Location = new System.Drawing.Point(458, 204);
             this.label_raw.Name = "label_raw";
             this.label_raw.Size = new System.Drawing.Size(29, 13);
             this.label_raw.TabIndex = 20;
@@ -230,7 +255,7 @@ namespace GenshinArtifactOCR
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(800, 652);
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.label_raw);
             this.Controls.Add(this.text_raw);
             this.Controls.Add(this.btn_OCR);
@@ -251,6 +276,7 @@ namespace GenshinArtifactOCR
             this.Controls.Add(this.text_Type);
             this.Controls.Add(this.text_Set);
             this.Controls.Add(this.image_preview);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.image_preview)).EndInit();
--- GenshinArtifactOCR/Form1.cs
+++ GenshinArtifactOCR/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 using Tesseract;
 
 namespace GenshinArtifactOCR
@@ -16,10 +19,11 @@ namespace GenshinArtifactOCR
         private TesseractEngine tessEngine;
         private Bitmap img_Raw;
         private Bitmap img_Filtered;
+
+        private static string appDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfo"; //Use WFInfo tesseract version for now
         public Form1()
         {
             InitializeComponent();
-            string appDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFInfo";
             tessEngine = new TesseractEngine(appDir + @"/tessdata", "en")
             {
                 DefaultPageSegMode = PageSegMode.SingleLine
@@ -27,11 +31,208 @@ namespace GenshinArtifactOCR
             img_Raw = new Bitmap(image_preview.Width, image_preview.Height);
             using (Graphics g = Graphics.FromImage(img_Raw))
             {
-                g.FillRectangle(Brushes.White, 0, 0, img_Raw.Width, img_Raw.Height);
+                g.FillRectangle(Brushes.Black, 0, 0, img_Raw.Width, img_Raw.Height);
+                g.FillRectangle(Brushes.White, img_Raw.Width / 8, img_Raw.Height / 8, img_Raw.Width * 6 / 8, img_Raw.Height * 6 / 8);
             }
             img_Filtered = new Bitmap(img_Raw);
             image_preview.Image = new Bitmap(img_Raw);
         }
 
+        private Bitmap filterAndGetArtifactArea(Bitmap img)
+        {
+            int[] cols = new int[img.Width];
+            BitmapData imgData = img_Filtered.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadWrite, img.PixelFormat);
+            int numBytes = Math.Abs(imgData.Stride) * imgData.Height;
+            byte[] imgBytes = new byte[numBytes];
+            Marshal.Copy(imgData.Scan0, imgBytes, 0, numBytes);
+            int PixelSize = 4; //ARGB, reverse order
+            for (int i = 0; i < numBytes; i += PixelSize)
+            {
+                int x = (i / PixelSize) % img.Width;
+                int y = (i / PixelSize - x) % img.Height;
+                if (imgBytes[i + 1] > 220 || (imgBytes[i] > 170 && imgBytes[i + 1] > 170 && imgBytes[i + 2] > 170))
+                {
+                    //Make Black
+                    imgBytes[i] = 0;
+                    imgBytes[i + 1] = 0;
+                    imgBytes[i + 2] = 0;
+                    imgBytes[i + 3] = 255;
+                    cols[x]++;
+                }
+                else
+                {
+                    //Make White
+                    imgBytes[i] = 255;
+                    imgBytes[i + 1] = 255;
+                    imgBytes[i + 2] = 255;
+                    imgBytes[i + 3] = 255;
+                }
+            }
+            Marshal.Copy(imgBytes, 0, imgData.Scan0, numBytes);
+            img.UnlockBits(imgData);
+
+            //Find artifact text columns
+            int edgewidth = 0;
+            while (cols[cols.Length - 1 - edgewidth] / (double)img.Height < 0.01)
+                edgewidth++;
+            int rightmost = cols.Length - edgewidth;
+            int leftmost = rightmost - edgewidth;
+            int misses = 0;
+            while (leftmost - misses > 0 && misses < edgewidth * 2)
+            {
+                if (cols[leftmost - misses] / (double)img.Height > 0.01)
+                {
+                    leftmost -= misses + 1;
+                    misses = 0;
+                }
+                else
+                {
+                    misses++;
+                }
+            }
+            leftmost -= edgewidth / 2;
+
+
+            //find artifact text rows
+            int[] rows = new int[img.Height];
+            for (int i = 0; i < numBytes; i += PixelSize)
+            {
+                int x = (i / PixelSize) % img.Width;
+                int y = (i / PixelSize - x) % img.Height;
+                if (x > leftmost  && x < rightmost && imgBytes[i] == 0)
+                {
+                    rows[y]++;
+                }
+            }
+
+            int top = 0;
+            int left = leftmost;
+            int width = rightmost - leftmost;
+            int height = img.Height;
+            Bitmap artifactArea = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(artifactArea))
+            {
+                Rectangle sourceRect = new Rectangle(left, top, width, height);
+                g.DrawImage(img, 0, 0, sourceRect, GraphicsUnit.Pixel);
+            }
+            return artifactArea;
+
+        }
+
+        private Bitmap CaptureScreenshot()
+        {
+            Bitmap img = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Size fullSize = new Size(img.Width, img.Height);
+            using (Graphics g = Graphics.FromImage(img))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, fullSize);
+            }
+            string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH-mm-ssff");
+            img.Save(appDir + @"\Debug\GenshinSC " + timestamp + ".png");
+            return img;
+        }
+
+        private Bitmap findGameArea(Bitmap full)
+        {
+            int minWidth = Screen.PrimaryScreen.Bounds.Width / 4;
+            for (int y = full.Height / 2; y > 0; y--)
+            {
+                int x = full.Width / 2;
+                int i_pos = 0;
+                int i_neg = 0;
+                Color pixel = full.GetPixel(x, y);
+
+                //explore right
+                while ( x + i_pos < full.Width*0.99 && pixel.R > 220 && pixel.G > 220 && pixel.B > 220) 
+                {
+                    i_pos++;
+                    pixel = full.GetPixel(x + i_pos, y);
+                }
+
+                if (i_pos == 0)
+                    continue;
+
+                pixel = full.GetPixel(x, y);
+                //explore left
+                while (x - i_neg > full.Width * 0.01 && pixel.R > 220 && pixel.G > 220 && pixel.B > 220)
+                {
+                    i_neg++;
+                    pixel = full.GetPixel(x - i_neg, y);
+                }
+
+                if (i_pos + i_neg < minWidth)
+                    continue;
+
+                int top = y + 1;
+                int left = x - i_neg + (i_pos + i_neg) / 2;
+                int width = (i_pos + i_neg) / 2;
+                int height = (full.Height - y + 1) * 2/3;
+                Bitmap gameArea = new Bitmap(width, height);
+                using (Graphics g = Graphics.FromImage(gameArea))
+                {
+                    Rectangle sourceRect = new Rectangle(left, top, width, height);
+                    g.DrawImage(full, 0, 0, sourceRect, GraphicsUnit.Pixel);
+                }
+
+                string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH-mm-ssff");
+                gameArea.Save(appDir + @"\Debug\GenshinSC_gameonly " + timestamp + ".png");
+                return gameArea;
+            }
+            return null;
+        }
+
+        private Bitmap LoadScreenshot()
+        {
+            Bitmap img = new Bitmap(1, 1);
+            // Using WinForms for the openFileDialog because it's simpler and much easier
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                openFileDialog.Filter = "image files (*.png)|*.png|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        foreach (string file in openFileDialog.FileNames)
+                        {
+                            img = new Bitmap(file);
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+            }
+            return img;
+        }
+
+        private void btn_Filter_Click(object sender, EventArgs e)
+        {
+            img_Filtered = new Bitmap(img_Raw);
+            img_Filtered = filterAndGetArtifactArea(img_Filtered);
+
+            image_preview.Image = new Bitmap(img_Filtered);
+        }
+
+        private void btn_capture_Click(object sender, EventArgs e)
+        {
+            Bitmap img;
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                img = LoadScreenshot();
+            } else
+            {
+                img = CaptureScreenshot();
+                img = findGameArea(img);
+            }
+            img_Raw = new Bitmap(img);
+            image_preview.Image = new Bitmap(img);
+        }
     }
 }
